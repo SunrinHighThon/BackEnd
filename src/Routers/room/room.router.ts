33b0a2c0b17a7b22a4a777a307252536ecb1df34
태민 <48385263+UniTaeMin@users.noreply.github.com>
2020-01-18 19:28:57 +0900

    SocketStart

--- src/Routers/room/room.router.ts
+++ src/Routers/room/room.router.ts
@@ -1,9 +1,10 @@
-import * as express from "express"
-import { Create,Join,Leave } from "./room.controller";
+import * as express from "express";
+import { Create, Join, Leave, Start } from "./room.controller";
 
 const router = express.Router();
 
-router.post("/create",Create);
+router.post("/create", Create);
 router.post("/join", Join);
-router.post("/leave",Leave);
-export default router
+router.post("/leave", Leave);
+router.post("/start", Start);
+export default router;
